dd1eb55a7b696227d55446e2818cb1be1ebcebbd
armavox <armavox@gmail.com>
2018-11-18 13:05:53 +0300

    pursuit_1_upd
    Preprocessing enchanced. default Ridge() make 1.0651

--- assignment6_medium_pursuit_1.py
+++ assignment6_medium_pursuit_1.py
@@ -10,7 +10,7 @@
 # 
 # 
 
-# In[1]:
+# In[3]:
 
 
 import os
@@ -19,10 +19,19 @@ from tqdm import tqdm_notebook, tqdm
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import metrics
 from sklearn.metrics import mean_absolute_error
-from scipy.sparse import csr_matrix, hstack
-from sklearn.linear_model import Ridge
+from scipy.sparse import csr_matrix, hstack, save_npz, load_npz
+from sklearn.linear_model import Ridge, RidgeCV
 import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
+
+
+# In[6]:
+
+
+PATH_TO_DATA = 'data/' # modify this if you need to
 
 
 # # Feature engineering
@@ -74,19 +83,19 @@ def read_json_line(line=None):
 
 # Extract features `content`, `published`, `title` and `author`, write them to separate files for train and test sets.
 
-# In[66]:
+# In[4]:
 
 
 import clean
 
 
-# In[4]:
+# In[116]:
 
 
 def extract_features_and_write(path_to_data,
                                inp_filename, is_train=True):
     
-    features = ['content', 'published', 'title', 'author', 'link_tags']
+    features = ['content', 'published', 'title', 'author']
     prefix = 'train' if is_train else 'test'
     feature_files = [open(os.path.join(path_to_data, 'generated',
                                        '{}_{}.txt'.format(prefix, feat)),
@@ -102,14 +111,14 @@ def extract_features_and_write(path_to_data,
             for key, file in zip(features, feature_files):
                 if key == 'content':
                     file.write(clean.cleaning(strip_tags(
-                        json_data[key].replace('\n', ' ').replace('\r', ' '))+'\n'))
+                        json_data[key].replace('\n', ' ').replace('\r', ' ')))+'\n')
                 elif key == 'published':
                     file.write(list(json_data[key].values())[0]+'\n')
                 elif key == 'author':
                     file.write(json_data[key]['url'] + '\n')
                 elif key == 'title':
-                    file.write(
-                        json_data[key].replace('\n', ' ').replace('\r', ' ')+'\n')
+                    file.write(clean.cleaning(
+                        json_data[key].replace('\n', ' ').replace('\r', ' '))+'\n')
                 else:
                     file.write(json_data[key]+'\n')
 
@@ -117,7 +126,7 @@ def extract_features_and_write(path_to_data,
         
 
 
-# In[35]:
+# In[48]:
 
 
 with open('data/train.json') as f:
@@ -125,25 +134,13 @@ with open('data/train.json') as f:
         jsond = read_json_line(f.readline())
 
 
-# In[38]:
-
-
-jsond['link_tags']
-
-
-# In[6]:
-
-
-PATH_TO_DATA = 'data/' # modify this if you need to
-
-
-# In[40]:
+# In[124]:
 
 
 extract_features_and_write(PATH_TO_DATA, 'train.json', is_train=True)
 
 
-# In[7]:
+# In[117]:
 
 
 extract_features_and_write(PATH_TO_DATA, 'test.json', is_train=False)
@@ -155,7 +152,7 @@ extract_features_and_write(PATH_TO_DATA, 'test.json', is_train=False)
 #     - Time features: publication hour, whether it's morning, day, night, whether it's a weekend
 #     - Bag of authors (i.e. One-Hot-Encoded author names)
 
-# In[9]:
+# In[125]:
 
 
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -164,7 +161,7 @@ from sklearn.preprocessing import OneHotEncoder
 
 # #### Datetime features
 
-# In[10]:
+# In[126]:
 
 
 X_datetime_train = pd.read_csv('data/generated/train_published.txt', 
@@ -177,7 +174,7 @@ X_datetime_test = pd.read_csv('data/generated/test_published.txt',
 X_datetime_train.shape, X_datetime_test.shape
 
 
-# In[11]:
+# In[127]:
 
 
 def add_datetime_features(df):
@@ -202,7 +199,7 @@ def add_datetime_features(df):
                 night.values.reshape(-1,1)]))
 
 
-# In[12]:
+# In[128]:
 
 
 X_datetime_train_sparse = add_datetime_features(X_datetime_train)
@@ -211,14 +208,14 @@ X_datetime_test_sparse = add_datetime_features(X_datetime_test)
 
 # #### Author features
 
-# In[13]:
+# In[129]:
 
 
 def getnick(path):
     return os.path.basename(os.path.normpath(path))
 
 
-# In[14]:
+# In[130]:
 
 
 X_authors_train = pd.read_csv(open('data/generated/train_author.txt'), 
@@ -227,13 +224,13 @@ X_authors_test = pd.read_csv(open('data/generated/test_author.txt'),
                         names=['url']).url.apply(getnick)
 
 
-# In[15]:
+# In[131]:
 
 
 X_authors_train.shape, X_authors_test.shape
 
 
-# In[16]:
+# In[132]:
 
 
 ohe = OneHotEncoder(handle_unknown='ignore')  # Ignore unknown authors in test
@@ -243,7 +240,7 @@ X_authors_test_ohe = ohe.transform(X_authors_test.values.reshape(-1, 1))
 
 # #### Title and Content features
 
-# In[67]:
+# In[133]:
 
 
 tf_idf_word = TfidfVectorizer(ngram_range=(1,2), analyzer='word',
@@ -254,33 +251,47 @@ tf_idf_char = TfidfVectorizer(ngram_range=(1,4), analyzer='char',
                               max_features=5000, strip_accents='unicode')
 
 
-# In[85]:
+# In[134]:
 
 
-get_ipython().run_cell_magic('time', '', "with open('data/generated/train_title.txt') as input_file:\n    X_title_train_tfidfword = tf_idf_word.fit_transform(input_file)\nwith open('data/generated/train_title.txt') as input_file:\n    X_title_train_tfidfchar = tf_idf_char.fit_transform(input_file)\n\nwith open('data/generated/test_title.txt') as input_file:\n    X_title_test_tfidfword = tf_idf_word.transform(input_file)\nwith open('data/generated/test_title.txt') as input_file:\n    X_title_test_tfidfchar = tf_idf_char.transform(input_file)\n    \nX_title_train_tfidfword.shape, X_title_test_tfidfword.shape")
+get_ipython().run_cell_magic('time', '', "with open('data/generated/train_title.txt') as input_file:\n    X_title_train_tfidfword = tf_idf_word.fit_transform(input_file)\nwith open('data/generated/train_title.txt') as input_file:\n    X_title_train_tfidfchar = tf_idf_char.fit_transform(input_file)\n\nwith open('data/generated/test_title.txt') as input_file:\n    X_title_test_tfidfword = tf_idf_word.transform(input_file)\nwith open('data/generated/test_title.txt') as input_file:\n    X_title_test_tfidfchar = tf_idf_char.transform(input_file)")
 
 
-# In[86]:
+# In[137]:
 
 
-X_title_train_tfidfword.shape, X_title_test_tfidfword.shape
+X_title_train_tfidfchar.shape, X_title_test_tfidfchar.shape
 
 
-# In[178]:
+# In[144]:
 
 
-get_ipython().run_cell_magic('time', '', "with open('data/generated/train_content.txt') as f:\n    X_content_train_tfidf = tf_idf_vecr.fit_transform(f)\nwith open('data/generated/test_content.txt') as f:\n    X_content_test_tfidf = tf_idf_vecr.transform(f)\n\nX_content_train_tfidf.shape, X_content_test_tfidf.shape")
+X_title_train_tfidf = hstack([X_title_train_tfidfword, X_title_train_tfidfchar])
+X_title_test_tfidf = hstack([X_title_test_tfidfword, X_title_test_tfidfchar])
 
 
-# In[179]:
+# In[143]:
 
 
-X_content_train_tfidf.shape, X_content_test_tfidf.shape
+get_ipython().run_cell_magic('time', '', "with open('data/generated/train_content.txt') as input_file:\n    X_content_train_tfidfword = tf_idf_word.fit_transform(input_file)\nwith open('data/generated/train_title.txt') as input_file:\n    X_content_train_tfidfchar = tf_idf_char.fit_transform(input_file)\n\nwith open('data/generated/test_content.txt') as input_file:\n    X_content_test_tfidfword = tf_idf_word.transform(input_file)\nwith open('data/generated/test_content.txt') as input_file:\n    X_content_test_tfidfchar = tf_idf_char.transform(input_file)")
+
+
+# In[145]:
+
+
+X_content_train_tfidfword.shape, X_content_test_tfidfword.shape
+
+
+# In[146]:
+
+
+X_content_train_tfidf = hstack([X_content_train_tfidfword, X_content_train_tfidfchar])
+X_content_test_tfidf = hstack([X_content_test_tfidfword, X_content_test_tfidfchar])
 
 
 # **Join all sparse matrices.**
 
-# In[267]:
+# In[147]:
 
 
 X_train_sparse = hstack([X_content_train_tfidf, X_title_train_tfidf,
@@ -289,12 +300,38 @@ X_test_sparse = hstack([X_content_test_tfidf, X_title_test_tfidf,
                         X_datetime_test_sparse, X_authors_test_ohe]).tocsr()
 
 
+# In[150]:
+
+
+import scipy
+
+
+# In[151]:
+
+
+scipy.sparse.save_npz('data/generated/X_train_sparse', X_train_sparse)
+scipy.sparse.save_npz('data/generated/X_test_sparse', X_test_sparse)
+
+
+# In[155]:
+
+
+X_train_sparse.shape, X_test_sparse.shape
+
+
+# In[4]:
+
+
+X_train_sparse = load_npz('data/generated/X_train_sparse.npz')
+X_test_sparse = load_npz('data/generated/X_test_sparse.npz')
+
+
 # # Training and Validating
 # ___
 
 # **Read train target and split data for validation.**
 
-# In[268]:
+# In[7]:
 
 
 train_target = pd.read_csv(os.path.join(PATH_TO_DATA, 'train_log1p_recommends.csv'), 
@@ -302,7 +339,7 @@ train_target = pd.read_csv(os.path.join(PATH_TO_DATA, 'train_log1p_recommends.cs
 y_train = train_target['log_recommends'].values
 
 
-# In[272]:
+# In[8]:
 
 
 train_part_size = int(0.7 * train_target.shape[0])
@@ -316,6 +353,8 @@ y_valid = y_train[train_part_size:]
 
 # **Train a simple Ridge model and check MAE on the validation set.**
 
+# #### Baseline
+
 # In[273]:
 
 
@@ -324,15 +363,77 @@ ridge_regr.fit(X_train_part_sparse, y_train_part)
 mean_absolute_error(y_valid, ridge_regr.predict(X_valid_sparse))
 
 
+# #### Enchanced trainset
+
+# In[ ]:
+
+
+ridge_regr = Ridge(random_state=42)
+ridge_regr.fit(X_train_part_sparse, y_train_part)
+mean_absolute_error(y_valid, ridge_regr.predict(X_valid_sparse))
+
+
+# In[20]:
+
+
+mean_absolute_error(y_valid, ridge_regr.predict(X_valid_sparse))
+
+
+# In[20]:
+
+
+mean_absolute_error(y_valid, ridge_regr.predict(X_valid_sparse))
+
+
+# #### Try RidgeCV
+
+# In[37]:
+
+
+kf = KFold(n_splits=3, shuffle=True, random_state=42)
+alphas = np.logspace(-2, 6, 10)
+ridge = Ridge(random_state=42)
+
+
+# In[15]:
+
+
+ridge.get_params()
+
+
+# In[41]:
+
+
+params = {'alpha': np.logspace(-2, 2, 30)}
+
+
+# In[42]:
+
+
+get_ipython().run_cell_magic('time', '', "grid = GridSearchCV(ridge, param_grid=params,\n                    scoring='neg_mean_absolute_error',\n                    cv=kf, n_jobs=-1, verbose=1)\ngrid.fit(X_train_part_sparse, y_train_part)")
+
+
+# In[44]:
+
+
+mean_absolute_error(y_valid, grid.predict(X_valid_sparse))
+
+
+# In[43]:
+
+
+grid.best_estimator_
+
+
 # **Train the same Ridge with all available data, make predictions for the test set and form a submission file.**
 
-# In[275]:
+# In[21]:
 
 
 get_ipython().run_cell_magic('time', '', 'y_submit = ridge_regr.fit(X_train_sparse, y_train).predict(X_test_sparse)')
 
 
-# In[306]:
+# In[22]:
 
 
 plt.hist(y_valid, bins=30, alpha=.5, color='red', label='true', range=(0,10));
@@ -342,7 +443,7 @@ plt.hist(y_train, bins=30, alpha=.15, color='blue', label='train')
 plt.legend();
 
 
-# In[283]:
+# In[23]:
 
 
 def write_submission_file(prediction, filename,
@@ -354,11 +455,11 @@ def write_submission_file(prediction, filename,
     submission.to_csv(filename)
 
 
-# In[284]:
+# In[24]:
 
 
 write_submission_file(y_submit, os.path.join(PATH_TO_DATA,
-                                                    'assignment6_medium_submission.csv'))
+                                                    'assignment6_medium_submission_pursuit_1.csv'))
 
 
 # **Now's the time for dirty Kaggle hacks. Form a submission file with all zeroes. Make a submission. What do you get if you think about it? How is it going to help you with modifying your predictions?**
@@ -373,36 +474,34 @@ write_submission_file(np.zeros_like(y_submit),
 
 # **Modify predictions in an appropriate way (based on your all-zero submission) and make a new submission.**
 
-# In[291]:
+# In[32]:
+
+
+y_submit.mean()
+
+
+# In[31]:
 
 
 mean_right = 4.33328
 
-
-# In[293]:
-
-
 diff = mean_right - y_submit.mean()
-
-
-# In[296]:
-
 
 diff
 
 
-# In[297]:
+# In[33]:
 
 
 ridge_test_pred_modif = y_submit + diff # You code here
 
 
-# In[298]:
+# In[34]:
 
 
 write_submission_file(ridge_test_pred_modif, 
                       os.path.join(PATH_TO_DATA,
-                                   'assignment6_medium_submission_with_hack.csv'))
+                                   'assignment6_medium_submission_pursuit_1_with_hack.csv'))
 
 
 # That's it for the assignment. Much more credits will be given to the winners in this competition, check [course roadmap](https://mlcourse.ai/roadmap). Do not spoil the assignment and the competition - don't share high-performing kernels (with MAE < 1.5).
